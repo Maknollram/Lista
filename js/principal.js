0397d4b7d2c24a4ce446cb9be49faa65e0d131d5
Marllon <01e2116>
2018-03-09 16:12:04 -0300

    inserção do nedb

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,5 @@
+window.bd = new Nedb({ filename: 'bd/lista.db', autoload: true })
+
 const app = new Vue({
     el: "#app",
     data: { 
@@ -13,18 +15,63 @@ const app = new Vue({
             // suffix: ' Reais',
             precision: 2,
             masked: false
-        }
+        },
+        novaLista: []
+    },
+    mounted(){
+        
     },
     computed:{
        total(){
            return this.linhas.reduce((total, l) => total + l.valor, 0)
        }
+       
     },
     methods:{
+        salvar(){
+            if (this.linhas[0].item != "" && this.linhas[0].valor != 0){
+                var confirmacao = true
+                bd.insert(this.linhas, function (err, doc){
+                    if (doc.lenght != 0){
+                        bd.find({}, function(err, docs){
+                            this.novaLista = docs
+                            console.log(docs)
+                            console.log(this.novaLista)
+                        })
+                    }
+                    if(err){
+                        confirmacao = false
+                        return
+                    }
+                })
+                if(confirmacao){
+                    swal({
+                        position: 'center',
+                        type: 'success',
+                        title: 'Lista salva com sucesso!',
+                        showConfirmButton: false,
+                        timer: 1500
+                    })
+                }else{
+                    swal({
+                        position: 'center',
+                        type: 'error',
+                        title: 'Ocorreu um erro, a lista não foi salva!',
+                        showConfirmButton: false,
+                        timer: 1500
+                    })
+                }
+            }else{
+                swal({
+                    position: 'center',
+                    type: 'error',
+                    title: 'Lista vazia não será salva!',
+                    showConfirmButton: false,
+                    timer: 1500
+                })
+            }
+        },
         incluirLinha(){
-        //     $.getJSON("buscarListaCentroCarga").done(response => {
-        //         this.incluir.listCentroCargaBancoDados = response.listaTodosCentroCarga
-        //    }) 
             this.linhas.push({id: this.linhas[this.linhas.length-1].id + 1, item: "", valor: 0})
         },
         removerLinha(id){
@@ -47,6 +94,9 @@ const app = new Vue({
 				buttonsStyling: false
 	        }).then((confirmacao) =>{
                 this.linhas = [{id: 1, item:"", valor: 0}]
+                bd.remove({}, { multi: true }, function (err, numRemoved) {
+                    this.novaLista = []
+                })
             });
         }
     }

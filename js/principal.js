9213815d27b6d31b7c17aaf61ccca9a10c9eb5b6
Marllon <01e2116>
2018-03-12 13:30:52 -0300

    remoção do bd

--- js/principal.js
+++ js/principal.js
@@ -16,29 +16,27 @@ const app = new Vue({
             precision: 2,
             masked: false
         },
-        novaLista: []
+        novaLista: [],
+        lista: []
     },
     mounted(){
-        
+        this.getDados()
     },
     computed:{
        total(){
            return this.linhas.reduce((total, l) => total + l.valor, 0)
        }
-       
     },
     methods:{
         salvar(){
             if (this.linhas[0].item != "" && this.linhas[0].valor != 0){
                 var confirmacao = true
-                bd.insert(this.linhas, function (err, doc){
-                    if (doc.lenght != 0){
-                        bd.find({}, function(err, docs){
-                            this.novaLista = docs
-                            console.log(docs)
-                            console.log(this.novaLista)
-                        })
-                    }
+                // for (let i = 0; i < this.linhas.length; i++ ){
+                //     if (this.linhas[i].id != this.novaLista[i].id){
+                //         this.lista.push({id: this.linhas[i].id, item: this.linhas[i].item, valor: this.linhas[i].valor})
+                //     }
+                // }
+                bd.insert(this.lista, function (err, doc){
                     if(err){
                         confirmacao = false
                         return
@@ -74,8 +72,25 @@ const app = new Vue({
         incluirLinha(){
             this.linhas.push({id: this.linhas[this.linhas.length-1].id + 1, item: "", valor: 0})
         },
+        getDados(){
+            bd.find({}, function(err, docs){
+                app.novaLista = docs
+                if(docs.length != 0)    {
+                    app.linhas = []
+                    for (let i = 0; i< docs.length; i++){
+                        app.linhas.push({id: docs[i].id, item: docs[i].item, valor: docs[i].valor})
+                    }
+                }
+            })
+        },
         removerLinha(id){
             this.linhas = this.linhas.filter(l => l.id !== id)
+            if (this.linhas.length == 0){
+                this.linhas = [{id: 1, item:"", valor: 0}]
+                return
+            }
+            bd.remove({id: id}, {}, function (err, numRemoved) {})
+            this.getDados()
         },
         limparLista(){
             swal({
@@ -95,7 +110,7 @@ const app = new Vue({
 	        }).then((confirmacao) =>{
                 this.linhas = [{id: 1, item:"", valor: 0}]
                 bd.remove({}, { multi: true }, function (err, numRemoved) {
-                    this.novaLista = []
+                    this.linhas = []
                 })
             });
         }
